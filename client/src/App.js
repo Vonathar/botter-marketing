8293051212e61a99ff52edcf962785a660f27caa
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2020-10-04 12:59:52 +0100

    Implement basic React app skeleton

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+    return (<div className="App">
+            <header className="App-header">
+                <p>TODO: Build client</p>
+            </header>
+        </div>);
+}
+
+export default App;
