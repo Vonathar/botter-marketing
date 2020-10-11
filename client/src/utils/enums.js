e7d177cb1a6e6a5ea832439a6cecb062516de96f
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2020-10-11 19:39:56 +0100

    Implement enums.js to store all enum-like objects.

---
+++ client/src/utils/enums.js
@@ -0,0 +1,9 @@
+/**
+ * @summary Exports Enum-like objects for constant values.
+ * @author  Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
+ */
+
+/** The legal query types for the BotterMarketing API. */
+export const QueryType = Object.freeze({
+  CAMPAIGN_INFO: "campaignInfo",
+});
