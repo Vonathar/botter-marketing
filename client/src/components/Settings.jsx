b7536e4e22b493bb4685c29639acd0e8949fd335
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2020-10-11 19:29:02 +0100

    Add JSDoc for Settings.jsx

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
 import Button from "./common/Button";
 import TextInput from "./common/TextInput";
+import PropTypes from "prop-types";
 
 /**
- * Renders the available menu settings for the currently selected option.
+ * Renders the available settings for the currently selected option.
  * @component
  */
 export default class Settings extends Component {
+  static propTypes = {
+    /** The title of the currently selected option. */
+    selectedOptionTitle: PropTypes.string.isRequired,
+    /** A function to update the projectName using the value of an Event object. */
+    setProjectName: PropTypes.func.isRequired,
+    /** A function to fetch resources from the BotterMarketing API. */
+    get: PropTypes.func.isRequired,
+  };
+
   /**
    * Returns the appropriate settings menu for each available option.
    * @return JSX that represents the settings in the DOM, or undefined if
    *         there are no settings available for the given selectedOptionTitle.
-   * */
+   */
   renderSettings = () => {
     switch (this.props.selectedOptionTitle) {
       case "Campaign info": {
@@ -23,29 +33,23 @@ export default class Settings extends Component {
   };
 
   /**
-   * Gets the text content of the input, then updates the state in the App component.
-   * */
-  textChangeHandler = (event, stateKey) => {
-    const value = event.target.value;
-    switch (stateKey) {
-      case "projectName":
-        this.props.setProjectName(value);
-    }
-  };
-
-  /**
    * Renders the settings menu for the "Campaign info" option.
    * @return JSX that represents the available settings for "Campaign info".
-   * */
+   */
   getCampaignInfoSettings = () => {
     return (
       <div className="app__settings">
         <TextInput
           label={"Project name"}
-          textChangeHandler={this.textChangeHandler}
+          handleTextChange={this.props.setProjectName}
           stateKey={"projectName"}
         />
-        <Button text={"START"} handleClick={() => {this.props.get("campaignInfo")}} />
+        <Button
+          text={"START"}
+          handleClick={() => {
+            this.props.get("campaignInfo");
+          }}
+        />
       </div>
     );
   };
