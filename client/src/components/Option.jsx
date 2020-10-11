25db9ff23a2e2df12487786028c7b37f4c8efd13
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2020-10-11 17:15:58 +0100

    Add JSDoc for Option.jsx

--- client/src/components/Option.jsx
+++ client/src/components/Option.jsx
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 /**
  * Renders a single clickable menu option.
  * @component
+ * @example
+ * <Option
+ *    icon="lens"
+ *    title="foo"
+ *    description="bar"
+ *    handleClick={myFunc}
+ *    selectedOptionTitle="Campaign Info"
+ * />
  */
 export default class Option extends Component {
+  static propTypes = {
+    /** The filename of the icon to use from the /public directory. */
+    icon: PropTypes.string.isRequired,
+    /** A short title for the option. */
+    title: PropTypes.string.isRequired,
+    /** A brief description of the purpose of the option. */
+    description: PropTypes.string.isRequired,
+    /** The event handler for click events on the entire component. */
+    handleOptionClick: PropTypes.func.isRequired,
+    /** The title of the option that is currently selected. */
+    selectedOptionTitle: PropTypes.string
+  };
+
   /**
    * Fetches the icon from the /public directory based on the value of prop.icon
-   * @return A string that can be directly assigned as a path to an image's src
+   * @return {String} the path to use as the icon's src
    */
   getIcon = () => {
     return process.env.PUBLIC_URL + `/images/${this.props.icon}.svg`;
@@ -15,13 +37,13 @@ export default class Option extends Component {
 
   /**
    * Dynamically builds the className for menu options, always returning the base class and
-   * optionally returning the selected state modifier.
+   * optionally adding the selected state modifier.
    * @return A String that can be directly assigned to className
    */
   getClassName = () => {
-    let classNames = "home__menu__options__option";
+    let classNames = "app__options__option";
     return this.props.selectedOptionTitle === this.props.title
-      ? classNames + " home__menu__options__option--selected"
+      ? classNames + " app__options__option--selected"
       : classNames;
   };
 
@@ -30,16 +52,18 @@ export default class Option extends Component {
       <div
         className={this.getClassName()}
         onClick={() => {
-          this.props.setOptionSelected(this.props.title);
+          this.props.handleOptionClick(this.props.title);
         }}
       >
         <img
-          className={"home__menu__options__option__icon"}
+          className={"app__options__option__icon"}
           src={this.getIcon()}
           alt={this.props.icon}
         />
-        <h1 className={"home__menu__options__option__title"}>{this.props.title}</h1>
-        <p className={"home__menu__options__option__description"}>{this.props.description}</p>
+        <h1 className={"app__options__option__title"}>{this.props.title}</h1>
+        <p className={"app__options__option__description"}>
+          {this.props.description}
+        </p>
       </div>
     );
   }
