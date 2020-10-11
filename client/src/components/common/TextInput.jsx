e4c350b6dfabce9a2c497d5c2a1e514cc4524bd7
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2020-10-11 19:07:50 +0100

    Remove stateKey prop from TextInput.jsx; add JSDoc.

--- client/src/components/common/TextInput.jsx
+++ client/src/components/common/TextInput.jsx
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 /**
- * Renders a text input with the given label in material design style.
+ * Renders a text input with a label in material style.
  * @component
+ * @example
+ * <TextInput
+ *    handleTextChange={myFunc}
+ *    label="bar"
+ * />
  */
 export default class TextInput extends Component {
+  static propTypes = {
+    /** The event handler for onchange events. */
+    handleTextChange: PropTypes.func.isRequired,
+    /** The label of the input. */
+    label: PropTypes.string.isRequired,
+  };
+
   render() {
     return (
       <div className="text-input">
-        <input onChange={(event => {
-            this.props.textChangeHandler(event, this.props.stateKey)
-        })} type="text" required />
+        <input
+          onChange={this.props.handleTextChange}
+          type="text"
+          required
+        />
         <span className="bar" />
         <label>{this.props.label}</label>
       </div>
