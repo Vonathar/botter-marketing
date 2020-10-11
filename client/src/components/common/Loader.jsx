cdead4dd7ac8fadb02cb4617abba590b11c607e5
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2020-10-11 17:42:13 +0100

    Add JSDoc in Loader component, remove unnecessary React.Fragment

--- client/src/components/common/Loader.jsx
+++ client/src/components/common/Loader.jsx
@@ -1,24 +1,28 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 /**
  * Renders a basic loader with an infinite animation.
  * @component
  */
 export default class Loader extends Component {
+  static propTypes = {
+    /** Indicates whether the component should be rendered. */
+    isActive: PropTypes.bool.isRequired,
+    /** The message below the loader. */
+    message: PropTypes.string.isRequired,
+  };
+
   render() {
     if (this.props.isActive) {
       return (
-        <React.Fragment>
-          {
-            <div className={"loader"}>
-              <div className={"loader__animation"} />
-              <div className={"loader__message"}>{this.props.message}</div>
-            </div>
-          }
-        </React.Fragment>
+        <div className={"loader"}>
+          <div className={"loader__animation"} />
+          <div className={"loader__message"}>{this.props.message}</div>
+        </div>
       );
     } else {
-      return null
+      return null;
     }
   }
 }
