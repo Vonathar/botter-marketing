05df8394cebabfef576e1e3ac7767b0c115a84a7
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2020-10-16 23:26:38 +0100

    Updates

---
+++ precache-manifest.4f2e3c27579aa2b438fb2aa18f9f4415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd45be41477cf92aa4836ad1dc35f7de",
+    "url": "/botter-marketing/index.html"
+  },
+  {
+    "revision": "559f709c369bda366f62",
+    "url": "/botter-marketing/static/css/main.c82e60be.chunk.css"
+  },
+  {
+    "revision": "b4c87171b4367b310772",
+    "url": "/botter-marketing/static/js/2.61fdb999.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/botter-marketing/static/js/2.61fdb999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "559f709c369bda366f62",
+    "url": "/botter-marketing/static/js/main.6b2b2ab9.chunk.js"
+  },
+  {
+    "revision": "5544953a030cd063c2fe",
+    "url": "/botter-marketing/static/js/runtime-main.91203e4e.js"
+  }
+]);
