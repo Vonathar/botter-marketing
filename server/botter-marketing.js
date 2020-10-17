233b1e4039a3d86e778f91ce8532832045811151
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2020-10-17 12:43:50 +0100

    Require .env at the beginning of main index.js; add option to start Firefox in headless mode.

--- server/botter-marketing.js
+++ server/botter-marketing.js
@@ -3,21 +3,23 @@
  * @author  Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
  */
 
-const webdriver = require("selenium-webdriver"), By = webdriver.By, until = webdriver.until;
-const pm = require("./pm");
-const errors = require("./errors");
-const enums = require("./enums");
-const {getCampaignInfo} = require("./gitlab");
 require("dotenv").config();
+const firefox = require('selenium-webdriver/firefox');
+const webdriver = require("selenium-webdriver");
+const pm = require("./pm");
+const {getCampaignInfo} = require("./gitlab");
 
 /**
  *  @return Promise - resolved after the driver starts
  *  @desc Creates a new Firefox driver and adds it to the global scope.
  */
 let createDriver = () => {
-    global.driver = new webdriver.Builder()
-        .forBrowser("firefox")
-        .build();
+  let options = new firefox.Options();
+  options.addArguments("-headless");
+  global.driver = new webdriver.Builder()
+    .forBrowser("firefox")
+    //.setFirefoxOptions(options)
+    .build();
 };
 
 module.exports = {getCampaignInfo};
