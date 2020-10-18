02120d9d9695fd52cde0a6e453c532618f07c56e
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2020-10-18 14:04:33 +0100

    Dynamically enable Button component via props; add --disabled state; add JSDoc.

--- client/src/components/common/Button.jsx
+++ client/src/components/common/Button.jsx
@@ -18,11 +18,38 @@ export default class Button extends Component {
     handleClick: PropTypes.func.isRequired,
     /** The visible text on the button. */
     text: PropTypes.string.isRequired,
+    /** Whether the button should be clickable. */
+    isActive: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    isActive: true,
+  };
+
+  /**
+   * Dynamically builds the className for the button, always returning the base
+   * class and optionally adding the 'disabled' state modifier.
+   * @return {String} the className for the Button.
+   */
+  getClassName = () => {
+    let className = "button";
+    return this.props.isActive ? className : className + " button--disabled";
+  };
+
+  /**
+   * Dynamically enables the onclick handler based on the value of props.isActive.
+   * @return {function | null} the onclick handler for the Button.
+   */
+  setHandleClickActive = () => {
+    return this.props.isActive ? this.props.handleClick : null;
   };
 
   render() {
     return (
-      <div className={"button"} onClick={this.props.handleClick}>
+      <div
+        className={this.getClassName()}
+        onClick={this.setHandleClickActive()}
+      >
         {this.props.text}
       </div>
     );
